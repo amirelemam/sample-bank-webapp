6674518e4bae2cb1301af02936159ed15a65f44b
Amir Elemam <amirelemam@gmail.com>
2021-02-20 14:57:53 -0300

    Update envs

--- backend/account-manager/src/db/index.js
+++ backend/account-manager/src/db/index.js
@@ -1,20 +1,31 @@
 'use strict';
 
-const connection = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || '',
-};
+const logger = require('../common/logger');
 
-if (process.env.NODE_ENV === 'test') {
-  connection.database = 'postgres';
+let connection;
+
+if (process.env.DB_CONN_STR) {
+  connection = process.env.DB_CONN_STR;
+} else {
+  connection = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'postgres',
+  };
 }
 
 const knex = require('knex')({
   client: 'pg',
   version: '7.2',
   connection,
-  pool: { min: 0, max: 7 },
+  pool: {
+    min: 2,
+    max: 10,
+    afterCreate: () => {
+      logger.info('Successfully connected to DB.');
+    },
+  },
 });
 
 module.exports = knex;
