01e89c2ba05265f202d002ebe89731e87641ee6c
Amir Elemam <amirelemam@gmail.com>
2021-05-02 03:26:33 -0300

    [ATS] Add import

--- backend/account-type-simulator/src/common/errors.js
+++ backend/account-type-simulator/src/common/errors.js
@@ -6,4 +6,8 @@ class GerericError extends Error {
   }
 }
 
+exports.BadRequestError = (
+  msg = 'Missing fields or input in incorrect format.',
+) => new GerericError(msg, 400);
+
 exports.InternalServerError = (msg = 'Internal Server Error.') => new GerericError(msg, 500);
